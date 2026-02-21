8a5c6704cf8fd66e62e11da27aab65e6e395d299
Tharasri <tharasri_23it58@kgkite.ac.in>
2026-02-21 20:54:12 +0530

    base

--- frontend/src/services/base.js
+++ frontend/src/services/base.js
@@ -1,5 +1,5 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
-
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 if (!BASE_URL) {
   throw new Error("VITE_API_URL missing");
 }
